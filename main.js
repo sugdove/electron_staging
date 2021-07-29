213244bb93a24926985b0dbceddcc4600700ac5a
DESKTOP-O5RNJJH\admin <suge@yadingdata.com>
2021-07-29 15:35:35 +0800

    electron项目打包构建 根据官网 然后存放了一个自己写的github中文网

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-29 11:29:16
- * @LastEditTime: 2021-07-29 14:21:56
+ * @LastEditTime: 2021-07-29 15:33:57
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \electron-app\main.js
